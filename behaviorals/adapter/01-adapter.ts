7c7955e3aa18cf53f78284c0d86d33ba019248ac
Jean Osco <willjean29@gmail.com>
2025-01-30 22:08:29 -0500

    feat(creationals): add adapter pattern

---
+++ behaviorals/adapter/01-adapter.ts
@@ -0,0 +1,12 @@
+/**
+ * ! Patrón Adapter
+ *  Permite que objetos con interfaces incompatibles trabajen juntos, también es muy
+ *  util para utilizar librerías de terceros en nuestra aplicación sin depender
+ *  directamente de ellas.
+ *
+ * * Es útil cuando se quiere reutilizar una clase que no tiene la interfaz que
+ * * necesitamos o cuando queremos crear una capa de abstracción para una librería
+ * * de terceros.
+ *
+ * https://refactoring.guru/es/design-patterns/adapter
+ */
