97ef87abe19e096d7efd05cf4964051d529e71cf
Jean Osco <willjean29@gmail.com>
2025-02-10 22:33:54 -0500

    feat(structurals): add flyweight pattern

---
+++ structurals/flyweight/01-flyweight.ts
@@ -0,0 +1,10 @@
+/**
+ * ! Patrón Flyweight
+ * Es un patrón de diseño estructural que nos permite usar objetos compartidos
+ * para soportar eficientemente grandes cantidades de objetos.
+ *
+ * * Es útil cuando necesitamos una gran cantidad de objetos y queremos reducir
+ * * la cantidad de memoria que utilizan.
+ *
+ * https://refactoring.guru/es/design-patterns/flyweight
+ */
